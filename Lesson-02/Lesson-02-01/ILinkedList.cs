68ac85d202da43d4c75cb14c62ca9db9afde11be
Verm-V <Verm-V@yandex.ru>
2021-01-18 00:37:12 +0600

    1-е задание 2-го урока выполнено

---
+++ Lesson-02/Lesson-02-01/ILinkedList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_02_01
+{
+    //Начальную и конечную ноду нужно хранить в самой реализации интерфейса
+    public interface ILinkedList
+    {
+        int GetCount(); // возвращает количество элементов в списке
+        void AddNode(int value);  // добавляет новый элемент списка
+        void AddNodeAfter(Node node, int value); // добавляет новый элемент списка после определённого элемента
+        void RemoveNode(int index); // удаляет элемент по порядковому номеру
+        void RemoveNode(Node node); // удаляет указанный элемент
+        void RemoveFirst(); // удаляет первую ноду в списке
+        void RemoveLast(); // удаляет последнюю ноду в списке
+        void ClearList(); // очищает список
+        Node FindNode(int searchValue); // ищет элемент по его значению
+        bool CheckNode(Node node); // проверяет есть ли такой элемент в списке
+        Node FindNodeByIndex(int index); // ищет элемент по его индексу
+        
+
+    }
+}
