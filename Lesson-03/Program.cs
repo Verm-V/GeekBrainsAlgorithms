adc3d0c02b7f1e48d2e43799dad799221aec9763
Verm-V <Verm-V@yandex.ru>
2021-01-19 16:08:32 +0600

    Переделал заготовку проекта к 3-му уроку под .netCore

--- Lesson-03/Program.cs
+++ Lesson-03/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Lesson_03
 {
@@ -10,6 +6,7 @@ namespace Lesson_03
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
         }
     }
 }
