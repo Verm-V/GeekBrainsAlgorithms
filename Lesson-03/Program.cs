469b9b453e730d250cf9130894950ae86fee9039
Verm-V <Verm-V@yandex.ru>
2021-01-20 18:04:03 +0600

    добавил конфиг для бенчмарка и причесал программу

--- Lesson-03/Program.cs
+++ Lesson-03/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 
 namespace Lesson_03
@@ -12,34 +14,126 @@ namespace Lesson_03
         }
     }
 
-    public class BechmarkClass
+
+    [Config(typeof(MyConfig))]
+
+    public class BenchmarkClass
     {
-        
+        #region ---- FIELDS ----
+
+        /// <summary>Минимальное возможное значение координат</summary>
+        private const int MIN = -1000;
+        /// <summary>Максимальное возможное значение координат</summary>
+        private const int MAX = -1000;
+        /// <summary>Количество элементов в массиве со случайными координатами</summary>
+        private const int ELEMENTS = 1000;
+        /// <summary>Указатель для перебора кординат в массиве</summary>
+        private int index = 0;
+
+        /// <summary>Массив содержащий случайные значения координат</summary>
+        private int[] randomCoordinates = new int[ELEMENTS];
+
+        #endregion
+
+        #region ---- CONFIG ----
+
+        /// <summary>
+        /// Конфиг бенчмарка
+        /// </summary>
+        private class MyConfig : ManualConfig
+        {
+            public MyConfig()
+            {
+                AddColumn(StatisticColumn.Max);
+                AddColumn(StatisticColumn.Median);
+
+            }
+        }
+
+        #endregion
+
+        #region ---- CONSTRUCTORS ----
+
+        /// <summary>
+        /// Конструктор заполняющий массив координат случайной информацией
+        /// </summary>
+        public BenchmarkClass()
+        {
+            ///Заполняем массив случайными числами
+            Random rnd = new Random();
+            for (int i = 0; i < randomCoordinates.Length; i++)
+            {
+                randomCoordinates[i] = rnd.Next(MIN, MAX);
+                index = rnd.Next(ELEMENTS);
+            }
+        }
+
+        #endregion
+
+        #region ---- ADDITIONAL METHODS ----
+
+        /// <summary>
+        /// Выдает два числа из массива координат и сдвигает его указатель на следующую позицию
+        /// </summary>
+        /// <returns>Кортеж с двумя координатами</returns>
+        public (float, float) giveMeCoordinates()
+        {
+            if (index > ELEMENTS - 4)
+                index = 0;
+            else
+                index += 2;
+            return (randomCoordinates[index], randomCoordinates[index + 1]);
+        }
+
+        #endregion
 
         #region ---- DATA TYPES ----
+
+        /// <summary>Точка с координатами виде класса со сзначениями X, Y во float</summary>
         public class PointClassFloat
         {
             public float X { get; set; }
             public float Y { get; set; }
+
+            public PointClassFloat((float x, float y) coord)
+            {
+                X = coord.x;
+                Y = coord.y;
+            }
         }
 
+        /// <summary>Точка с координатами виде структуры со сзначениями X, Y во float</summary>
         public struct PointStructFloat
         {
             public float X;
             public float Y;
 
+            public PointStructFloat((float x, float y) coord)
+            {
+                X = coord.x;
+                Y = coord.y;
+            }
         }
+        /// <summary>Точка с координатами виде структуры со сзначениями X, Y в double</summary>
         public struct PointStructDouble
         {
             public double X;
             public double Y;
+            public PointStructDouble((double x, double y) coord)
+            {
+                X = coord.x;
+                Y = coord.y;
+            }
 
         }
 
         #endregion
 
-        #region ---- METHODS ----
-
+        #region ---- TESTED METHODS ----
+        /// <summary>Рассчет расстояния между двумя точками реализованными классов во float</summary>
+        /// <param name="pointOne">Точка 1</param>
+        /// <param name="pointTwo">Точка 2</param>
+        /// <returns>Расстояние между точками</returns>
         public float PointDistanceClassFloat(PointClassFloat pointOne, PointClassFloat pointTwo)
         {
             float x = pointOne.X - pointTwo.X;
@@ -47,6 +141,10 @@ namespace Lesson_03
             return MathF.Sqrt((x * x) + (y * y));
         }
 
+        /// <summary>Рассчет расстояния между двумя точками реализованными структурами во float</summary>
+        /// <param name="pointOne">Точка 1</param>
+        /// <param name="pointTwo">Точка 2</param>
+        /// <returns>Расстояние между точками</returns>
         public float PointDistanceStructFloat(PointStructFloat pointOne, PointStructFloat pointTwo)
         {
             float x = pointOne.X - pointTwo.X;
@@ -54,6 +152,10 @@ namespace Lesson_03
             return MathF.Sqrt((x * x) + (y * y));
         }
 
+        /// <summary>Рассчет расстояния между двумя точками реализованными структурами в double</summary>
+        /// <param name="pointOne">Точка 1</param>
+        /// <param name="pointTwo">Точка 2</param>
+        /// <returns>Расстояние между точками</returns>
         public double PointDistanceStructDouble(PointStructDouble pointOne, PointStructDouble pointTwo)
         {
             double x = pointOne.X - pointTwo.X;
@@ -61,46 +163,50 @@ namespace Lesson_03
             return Math.Sqrt((x * x) + (y * y));
         }
 
+        /// <summary>Рассчет квадрата расстояния между двумя точками реализованными структурами во float</summary>
+        /// <param name="pointOne">Точка 1</param>
+        /// <param name="pointTwo">Точка 2</param>
+        /// <returns>Квадрат расстояния между точками</returns>
         public float PointDistanceShortStructFloat(PointStructFloat pointOne, PointStructFloat pointTwo)
         {
             float x = pointOne.X - pointTwo.X;
             float y = pointOne.Y - pointTwo.Y;
-            return (x * x) + (y * y);
+            return ((x * x) + (y * y));
         }
 
         #endregion
 
         #region ---- BENCHMARKS ----
 
-        [Benchmark]
+        [Benchmark(Description = "Расстояние через классы float", Baseline = true)]
         public void TestPointDistanceClassFloat()
         {
-            PointClassFloat pointOne = new PointClassFloat() { X = 0, Y = 0 };
-            PointClassFloat pointTwo = new PointClassFloat() { X = 3, Y = 4 };
+            PointClassFloat pointOne = new PointClassFloat(giveMeCoordinates());
+            PointClassFloat pointTwo = new PointClassFloat(giveMeCoordinates());
             PointDistanceClassFloat(pointOne, pointTwo);
         }
 
-        [Benchmark]
+        [Benchmark(Description = "Расстояние через структуры float")]
         public void TestPointDistanceStructFloat()
         {
-            PointStructFloat pointOne = new PointStructFloat() { X = 0, Y = 0 };
-            PointStructFloat pointTwo = new PointStructFloat() { X = 3, Y = 4 };
+            PointStructFloat pointOne = new PointStructFloat(giveMeCoordinates());
+            PointStructFloat pointTwo = new PointStructFloat(giveMeCoordinates());
             PointDistanceStructFloat(pointOne, pointTwo);
         }
 
-        [Benchmark]
+        [Benchmark(Description = "Расстояние через структуры double")]
         public void TestPointDistanceStructDouble()
         {
-            PointStructDouble pointOne = new PointStructDouble() { X = 0, Y = 0 };
-            PointStructDouble pointTwo = new PointStructDouble() { X = 3, Y = 4 };
+            PointStructDouble pointOne = new PointStructDouble(giveMeCoordinates());
+            PointStructDouble pointTwo = new PointStructDouble(giveMeCoordinates());
             PointDistanceStructDouble(pointOne, pointTwo);
         }
 
-        [Benchmark]
+        [Benchmark(Description = "Квадрат расстояния через структуры float")]
         public void TestPointDistanceShortStructFloat()
         {
-            PointStructFloat pointOne = new PointStructFloat() { X = 0, Y = 0 };
-            PointStructFloat pointTwo = new PointStructFloat() { X = 3, Y = 4 };
+            PointStructFloat pointOne = new PointStructFloat(giveMeCoordinates());
+            PointStructFloat pointTwo = new PointStructFloat(giveMeCoordinates());
             PointDistanceShortStructFloat(pointOne, pointTwo);
         }
 
