a551e8a6d909a03c7f9f619a5f046c927d167a69
Verm-V <Verm-V@yandex.ru>
2021-01-25 17:11:48 +0600

    Добавлены описания для методов, доработан вывод на экран

--- Lesson-04/Lesson-04-02/BTree.cs
+++ Lesson-04/Lesson-04-02/BTree.cs
@@ -18,6 +18,28 @@ namespace Lesson_04_02
         /// <summary>Высота дерева</summary>
         private int height;
 
+        #region ---- PROPERTIES ----
+
+        /// <summary>Количество узлов в дереве</summary>
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        /// <summary>Количество уровней в дереве</summary>
+        public int Height
+        {
+            get
+            { 
+                return height; 
+            } 
+        }
+
+        #endregion
+
         #region ---- CONSTRUCTORS ----
 
         public BTree()
@@ -38,23 +60,29 @@ namespace Lesson_04_02
 
         #region ---- INTERFACE ----
 
+        /// <summary>
+        /// Добавляет новый узел в дерево
+        /// </summary>
+        /// <param name="value">Значение хранимое в новом узле</param>
         public void AddNode(int value)
         {
-            if (root == null)
+            if (root == null)//Если дерево пустое то добавляем корневой узел
             {
                 root = new Node(value);
                 count = 1;
                 height = 1;
             }
-            else
+            else//Иначе, рекурсивно ищем позицию для добавления узла
             {
                 int currentHeight = 1;
-                currentHeight = AddNodeAfter(root, value, currentHeight);
-                if(height<currentHeight) height = currentHeight;
+                currentHeight = AddChildNode(root, value, currentHeight);
+                if(height<currentHeight) height = currentHeight;//Если при добавлении изменилась высота дерева, то корректируем соответствующее поле.
             }
         }
 
-
+        /// <summary>
+        /// Удаляет все элементы из дерева
+        /// </summary>
         public void ClearTree()
         {
             root = null;
@@ -62,15 +90,29 @@ namespace Lesson_04_02
             height = 0;
         }
 
+        /// <summary>
+        /// Ищет наличие узла в дереве с заданным значением
+        /// </summary>
+        /// <param name="Value">Искомое значение</param>
+        /// <returns>true, если узел с таким значением есть в дереве.</returns>
         public bool FindNOde(int Value)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Возвращает количество узлов в дереве
+        /// </summary>
+        /// <returns>Количество узлов в дереве</returns>
         public int GetCount()
         {
             return count;
         }
+        
+        /// <summary>
+        /// Возвращает количество уровней в дереве
+        /// </summary>
+        /// <returns>Количество уровней в дереве</returns>
         public int GetHeight()
         {
             return height;
@@ -83,35 +125,49 @@ namespace Lesson_04_02
 
         #endregion
 
+        /// <summary>
+        /// Выводит структуру дерева в консоль.
+        /// </summary>
         public void Print()
         {
-            root.PrintPretty("", Node.NodePosition.center, true, false);
+            if(root != null)
+                root.PrintNode("", Node.NodePosition.center, true, false);
+            else
+                Console.WriteLine("Tree is empty.");
         }
 
 
-        private int AddNodeAfter(Node node, int value, int currentHeight)
+        /// <summary>
+        /// Добавляет потомка для указанного узла дерева
+        /// Если место уже занято, то переходит на следующий уровень
+        /// </summary>
+        /// <param name="node">Узел после ко</param>
+        /// <param name="value">Значение для нового узла</param>
+        /// <param name="currentHeight">Уровень дерева с которого пришел запрос на добавление</param>
+        /// <returns>Текущий уровень дерева</returns>
+        private int AddChildNode(Node node, int value, int currentHeight)
         {
             if(value <= node.Data)
             {
                 if (node.Left == null)
                 {
-                    node.Left = new Node(value);
+                    node.Left = new Node(value, currentHeight, node);
                     count++;
                 }
                 else
                 {
-                    currentHeight = AddNodeAfter(node.Left, value, currentHeight);
+                    currentHeight = AddChildNode(node.Left, value, currentHeight+1);
                 }
             }
             else
             {
                 if (node.Right == null)
                 {
-                    node.Right = new Node(value);
+                    node.Right = new Node(value, currentHeight, node);
                     count++;
                 }
                 else
-                    currentHeight = AddNodeAfter(node.Right, value, currentHeight);
+                    currentHeight = AddChildNode(node.Right, value, currentHeight+1);
             }
             return currentHeight+1;
         }
--- Lesson-04/Lesson-04-02/Node.cs
+++ Lesson-04/Lesson-04-02/Node.cs
@@ -6,26 +6,32 @@ using System.Threading.Tasks;
 
 namespace Lesson_04_02
 {
-	/// <summary>Узел двоичного дерева</summary>
-	public class Node
-	{
-		/// <summary>Данные содержащиеся в узле дерева</summary>
-		public int Data { get; set; }
-		/// <summary>Ссылка на левого потомка</summary>
-		public Node Left { get; set; }
-		/// <summary>Ссылка на правого потомка</summary>
-		public Node Right { get; set; }
-		/// <summary>Ссылка на родителя</summary>
-		public Node Parent { get; set; }
+    /// <summary>Узел двоичного дерева</summary>
+    public class Node
+    {
+        /// <summary>Данные содержащиеся в узле дерева</summary>
+        public int Data { get; set; }
+        /// <summary>Ссылка на левого потомка</summary>
+        public Node Left { get; set; }
+        /// <summary>Ссылка на правого потомка</summary>
+        public Node Right { get; set; }
+        /// <summary>Ссылка на родителя</summary>
+        public Node Parent { get; set; }
+        /// <summary>Ранг узла в дереве</summary>
+        public int Rank { get; set; }
 
-		public Node(int data, Node parent = null)
+        public Node(int data, int rank = 0, Node parent = null)
         {
-			Data = data;
-			Parent = parent;
+            Data = data;
+            Parent = parent;
+            Rank = rank;
         }
 
         #region ---- PRINT METHODS ----
 
+        /// <summary>
+        /// Позиция узла относительно корня
+        /// </summary>
         public enum NodePosition
         {
             left,
@@ -33,6 +39,11 @@ namespace Lesson_04_02
             center
         }
 
+        /// <summary>
+        /// Печатает значение узла
+        /// </summary>
+        /// <param name="value">Печатаемое значение, "-" если узел отсутствует</param>
+        /// <param name="nodePostion">Позиция узла относительно корня</param>
         private void PrintValue(string value, NodePosition nodePostion)
         {
             switch (nodePostion)
@@ -44,32 +55,52 @@ namespace Lesson_04_02
                     PrintRightValue(value);
                     break;
                 case NodePosition.center:
+                    Console.ForegroundColor = ConsoleColor.Yellow;
                     Console.WriteLine(value);
+                    Console.ForegroundColor = ConsoleColor.Gray;
                     break;
                 default:
                     throw new NotImplementedException();
             }
         }
+        
+        /// <summary>
+        /// Печать левой ветки узла
+        /// </summary>
+        /// <param name="value">Печатаемое значение, "--" если узел отсутствует</param>
         private void PrintLeftValue(string value)
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.Write("L:");
-            Console.ForegroundColor = (value == "-") ? ConsoleColor.Red : ConsoleColor.Yellow;
+            Console.ForegroundColor = (value == "--") ? ConsoleColor.Red : ConsoleColor.Yellow;
             Console.WriteLine(value);
             Console.ForegroundColor = ConsoleColor.Gray;
         }
 
+        /// <summary>
+        /// Печать правой ветки узла
+        /// </summary>
+        /// <param name="value">Печатаемое значение, "--" если узел отсутствует</param>
         private void PrintRightValue(string value)
         {
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.Write("R:");
-            Console.ForegroundColor = (value == "-") ? ConsoleColor.Red : ConsoleColor.Yellow;
+            Console.ForegroundColor = (value == "--") ? ConsoleColor.Red : ConsoleColor.Yellow;
             Console.WriteLine(value);
             Console.ForegroundColor = ConsoleColor.Gray;
         }
 
-        public void PrintPretty(string indent, NodePosition nodePosition, bool last, bool empty)
+
+        /// <summary>
+        /// Рекурсивно выводит на в консоль содержимое бинарного дерева
+        /// </summary>
+        /// <param name="indent">Отступ для печати значения</param>
+        /// <param name="nodePosition">Положение ноды относительно корня</param>
+        /// <param name="last">true, если это последний узел на этом уровне</param>
+        /// <param name="empty">true, если лист пустой</param>
+        public void PrintNode(string indent, NodePosition nodePosition, bool last, bool empty)
         {
+            System.Threading.Thread.Sleep(100);//небольшая задержка для наглядности работы алгоритма
 
             Console.Write(indent);
             if (last)
@@ -79,25 +110,25 @@ namespace Lesson_04_02
             }
             else
             {
-                Console.Write("├─");
-                indent += "| ";
+                Console.Write("├─");//│ ├ └ ─
+                indent += "│ ";
             }
 
-            var stringValue = empty ? "-" : Data.ToString();
+            var stringValue = empty ? "--" : Data.ToString();
             PrintValue(stringValue, nodePosition);
 
             if (!empty && (this.Left != null || this.Right != null))
             {
 
                 if (this.Right != null)
-                    this.Right.PrintPretty(indent, NodePosition.right, false, false);
+                    this.Right.PrintNode(indent, NodePosition.right, false, false);
                 else
-                    PrintPretty(indent, NodePosition.right, false, true);
+                    PrintNode(indent, NodePosition.right, false, true);
 
                 if (this.Left != null)
-                    this.Left.PrintPretty(indent, NodePosition.left, true, false);
+                    this.Left.PrintNode(indent, NodePosition.left, true, false);
                 else
-                    PrintPretty(indent, NodePosition.left, true, true);
+                    PrintNode(indent, NodePosition.left, true, true);
             }
         }
 
